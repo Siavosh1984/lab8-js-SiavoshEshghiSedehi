43e7e8733665d45b4afdc8eaac302212c8557833
Siavosh Eshghi Sedehi <n01602236@humber.ca>
2023-03-17 03:29:41 -0400

    first commit

---
+++ lab8-1-countdown.js
@@ -0,0 +1,50 @@
+﻿/* LAB 8-1 - FINAL COUNTDOWN!! */
+
+
+//create page load listener
+
+window.onload = pageReady
+
+
+//create page load function
+function pageReady(){
+
+	//create variables for required HTML elements
+	var todayDate = document.getElementById("todayData");
+	var dueDate = document.getElementById ("finalData");
+	var difDateShow = document.getElementById("dueData")
+	var myDate = new Date();
+	var dueDate_ = new Date("April 21, 2023 18:00:00");
+	var difTime = Math.floor((dueDate_ - myDate) / 86400000);
+	todayDate.innerHTML= myDate.toDateString();
+
+	dueDate.innerHTML = dueDate_.toDateString();
+
+	
+
+	if (dueDate_<myDate){
+
+		var timelabelClass = document.getElementsByClassName("timelabel")
+	    timelabelClass[2].style.display= "none";
+	    timelabelClass[3].style.display= "none";
+		difDateShow.innerHTML= "The Deadline for the Final Assignment has passed!"
+		}
+    else{
+
+
+	//create variables for now date and due date
+	
+	//CONVERT TO UTC AND SUBTRACT TO GET TIME DIFFERENCE
+
+	
+	
+	//CONVERT TIME DIFFERENCE TO WHOLE NUMBER OF DAYS
+	
+	//OUTPUT NOW DATE & DUE DATE TO HTML PAGE
+	difDateShow.innerHTML= difTime;
+	
+	//LOGIC TO CHECK IF DUE DATE HAS PASSED, AND OUPUT APPROPRIATE MESSAGE TO HTML PAGE
+
+    }
+
+	};
---
+++ lab8-2-clock.js
@@ -0,0 +1,91 @@
+﻿/* LAB 8.2 - STOP TIME */
+window.onload = timer_
+
+function timer_(){
+
+
+
+
+
+//create page load listener
+
+//create page load function
+
+	//create variables for required HTML elements
+	var hour_ = document.getElementById("hoursOut");
+	var minutes_ = document.getElementById("minsOut");
+	var seconds_ = document.getElementById("secsOut")
+	
+	//create time variable so all functions have access to it
+	
+	
+	/*setInterval(myTimer, 1000);
+
+function myTimer() {
+  var d = new Date();
+  document.getElementById("hoursOut").innerHTML = d.toLocaleTimeString();
+}*/
+
+
+
+
+    function time_calculator(){
+	var myDate = new Date();
+
+	if (myDate.getHours()<10){
+
+		hour_.innerHTML = "0" + myDate.getHours()
+	}
+	else {hour_.innerHTML =  myDate.getHours()};
+
+	if (myDate.getMinutes()<10){
+
+		minutes_.innerHTML = "0" + myDate.getMinutes()
+	}
+	else {minutes_.innerHTML =  myDate.getMinutes()};
+
+	if (myDate.getSeconds()<10){
+
+		seconds_.innerHTML = "0" + myDate.getSeconds()
+	}
+	else {seconds_.innerHTML =  myDate.getSeconds()};
+
+	
+	/*minutes_.innerHTML = myDate.getMinutes();
+	seconds_.innerHTML = myDate.getSeconds();*/
+    };
+
+	
+	
+	//CREATE FUNCTION THAT DISPLAYS THE TIME
+	var startBtn = document.getElementById("btnStart");
+	var stoptBtn = document.getElementById("btnStop");
+	var startPoint =null;
+	//CREATE FUNCTION TO START THE CLOCK.
+	function timeStart(){
+     startPoint = setInterval(time_calculator,1000);
+	
+	};
+	
+	//CREATE FUNCTION TO STOP THE CLOCK
+	
+	function timeStop(){
+
+    clearInterval(startPoint);
+	
+	
+	};
+
+	// SET EVENT LISTENERS
+	var startBtn = document.getElementById("btnStart");
+	var stoptBtn = document.getElementById("btnStop");
+
+	startBtn.onclick = timeStart
+	stoptBtn.onclick = timeStop
+
+	
+
+
+
+
+};
